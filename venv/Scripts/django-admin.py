75a34def22b9c91d9b596b0110dd3b0af7efa93c
Mudit Tripathi <mt2213@gmail.com>
2020-05-12 16:51:24 +0530

    initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\mudittripathi\vsc_projects\dev_real_state\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
